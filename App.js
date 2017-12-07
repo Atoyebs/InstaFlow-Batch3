f8b53302e1795d5e17b567f0f77979c2161cef28
KaiTechStudent5 <oremadeolabisi@gmail.com>
2017-12-07 12:19:40 +0100

    added a whole bunch of images as well as linter error checking to make the code more idiot proof

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,11 +16,11 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+};
